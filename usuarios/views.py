432eabb1b1edb561f4f7875c6a1828c11ca61ce0
zykor <enrique.wx@gmail.com>
2013-06-14 21:46:14 -0500

    mas

--- usuarios/views.py
+++ usuarios/views.py
@@ -17,35 +17,35 @@ from forms import SignupForm
 
 
 def nuevoUserView(request):
-	if request.method == 'POST':
-		formulario = SignupForm(request.POST)
-		if formulario.is_valid():
-			username = formulario.cleaned_data['username']
-			password = formulario.cleaned_data['password']
-			email = formulario.cleaned_data['email']
-			new_user = User.objects.create_user(username, email ,password)
-			new_user.is_active=True
-			new_user.is_staff=False
-			new_user.is_superuser=False
-			new_user.first_name=formulario.cleaned_data['first_name']
-			new_user.save()
-			return HttpResponseRedirect('/')
+	if request.user.is_authenticated():
+		return redirect('/')
 	else:
-		mensaje ="Los datos no son validos"
-		formulario =SignupForm()
-	return render_to_response('usuarios/registro.html',{'formulario': formulario }, \
-			context_instance=RequestContext(request))
+		if request.method == 'POST':
+			formulario = SignupForm(request.POST)
+			if formulario.is_valid():
+				username = formulario.cleaned_data['username']
+				password = formulario.cleaned_data['password']
+				email = formulario.cleaned_data['email']
+				new_user = User.objects.create_user(username, email ,password)
+				new_user.is_active=True
+				new_user.is_staff=False
+				new_user.is_superuser=False
+				new_user.first_name=formulario.cleaned_data['first_name']
+				new_user.save()
+				return HttpResponseRedirect('/')
+		else:
+			mensaje ="Los datos no son validos"
+			formulario =SignupForm()
+		return render_to_response('usuarios/registro.html',{'formulario': formulario }, \
+				context_instance=RequestContext(request))
 
 
 def login(request):
-	print 'hola'
 	if request.user.is_authenticated():
-		print 'hola'
 		return redirect('/')
 	else:
 		username = request.POST['username']
 		password = request.POST['password']
-		print 'hola'
 		user = authenticate(username=username, password=password)
 		if user is not None:
 			if user.is_active:
